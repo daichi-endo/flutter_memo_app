6672fb133a092ffb30dc9ad56235f0421cc8acd2
Daichi <endo-d@someru.co.jp>
2021-07-19 23:28:12 +0900

    Started development of flutter memo app

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:my_app2/pages/top_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: TopPage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+//todo アプリのタイトルを変更
